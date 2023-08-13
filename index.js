bc61112ec2e3fd679410e1f16703b7e4de6c52f3
IbrahimAli13 <ibraheemsaid200@gmail.com>
2023-08-13 23:04:09 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const UserModel = require('./models/Users');
+
+
+
+
+mongoose.connect('mongodb+srv://ibrahim:ibFA19041310@cluster0.qrrbu8u.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+    console.log('Connected')
+}).catch((err)=>{
+console.log('err')
+})
+
+
+
+app.get('/', async(req,res)=>{
+   const user = await UserModel.find()
+   res.json (user)
+})
+
+app.listen(3001 ,()=>{
+
+console.log('server is working')
+})
